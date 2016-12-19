57532905add7656693dc99e0c0eb24c900a09df5
pradeepgain <pgowrishetty@sysgain.com>
2016-12-19 14:05:08 +0530

    continuous logs

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function(req, res) {
+
     res.send('Hello world\n');
 });
+
+for (var i = 0; i <= 10; i++) {
+    (function(ind) {
+        setTimeout(function() { console.log('Mock Log number:', ind); }, 1000 + (3000 * ind));
+    })(i);
+}
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
