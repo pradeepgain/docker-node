2aefde89faee4c31a1807cc211ce8f94c6a79268
pradeepgain <pgowrishetty@sysgain.com>
2016-12-19 16:16:37 +0530

    change updated

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ app.get('/', function(req, res) {
     res.send('Hello world\n');
 });
 
+
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
 for (var i = 0; i <= 10; i++) {
     (function(ind) {
         setTimeout(function() { console.log('Mock Log number:', ind); }, 1000 + (3000 * ind));
     })(i);
 }
-
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
